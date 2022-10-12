faf03bacc99e2ba0d29a42b6858c6e06c55abafa
angga pcaw <anggapcaw@anggas-MacBook-Air.local>
2022-10-12 13:54:58 +0700

    heroku

--- config/allowOrigin.js
+++ config/allowOrigin.js
@@ -1,7 +1,7 @@
 const allowedOrigins = [
   "http://localhost:3000",
   "https://japa-app.digiteknoindo.my.id",
-  "https://testingbengkel.herokuapp.com",
+  "https://master.d30ib0nn2r6x2t.amplifyapp.com",
 ];
 
 module.exports = allowedOrigins;
