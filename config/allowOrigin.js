f9a4f3c2702134cae85f9d48dec58ab633852a98
angga pcaw <anggapcaw@anggas-MacBook-Air.local>
2022-10-20 10:28:13 +0700

    update

--- config/allowOrigin.js
+++ config/allowOrigin.js
@@ -2,7 +2,8 @@ const allowedOrigins = [
   "http://localhost:3000",
   "https://japa-app.digiteknoindo.my.id",
   "https://master.d30ib0nn2r6x2t.amplifyapp.com",
-  "https://testingbengkel.herokuapp.com"
+  "https://testingbengkel.herokuapp.com",
+  "http://fereact.s3-website-ap-northeast-1.amazonaws.com",
 ];
 
 module.exports = allowedOrigins;
