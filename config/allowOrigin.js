e43b425d1caa461b51104615735c4919563ea2d2
angga pcaw <anggapcaw@anggas-MacBook-Air.local>
2022-10-21 10:40:30 +0700

    origin

--- config/allowOrigin.js
+++ config/allowOrigin.js
@@ -1,8 +1,9 @@
 const allowedOrigins = [
   "http://localhost:3000",
-  "https://japa-app.digiteknoindo.my.id",
   "https://master.d30ib0nn2r6x2t.amplifyapp.com",
   "https://testingbengkel.herokuapp.com",
+  "http://54.65.89.147/digi/api/v1/employe",
+  "http://54.65.89.147/digi/api/v1/",
   "http://fereact.s3-website-ap-northeast-1.amazonaws.com",
 ];
 
