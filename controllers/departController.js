fb0880275dacb680e58bf0dbed68ecad71a47ee5
angga pcaw <anggapcaw@anggas-MacBook-Air.local>
2022-12-21 20:02:55 +0700

    update

--- controllers/departController.js
+++ controllers/departController.js
@@ -17,6 +17,7 @@ const getDepart = async (req, res) => {
     );
     const totalRows = await model.departemen.count();
     const results = await model.departemen.findAll({
+      attributes: ["id","kodedep", ["namadep", "name"]],
       where: {
         [Op.or]: [
           {
